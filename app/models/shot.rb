66043162913cfbc005a6d48f0d12b5db1148dc97
Yauhen <yauheni@MacBook-Pro-Yauh.local>
2020-03-13 01:58:29 +0300

    added MAX_PINS const

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,5 +1,7 @@
 class Shot < ApplicationRecord
+  MAX_PINS = 10.freeze
+
   belongs_to :frame
 
-  validates_numericality_of :pins, less_than_or_equal_to: 10
+  validates_numericality_of :pins, less_than_or_equal_to: MAX_PINS
 end
--- spec/models/shot_spec.rb
+++ spec/models/shot_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Shot, type: :model do
   it { should belong_to(:frame) }
 
-  it { should validate_numericality_of(:pins).is_less_than_or_equal_to(10) }
+  it { should validate_numericality_of(:pins).is_less_than_or_equal_to(Shot::MAX_PINS) }
 end
