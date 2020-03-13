b25de1e2c2d3b30b79abc50858b2ae4c63351d5f
Yauhen <yauheni@MacBook-Pro-Yauh.local>
2020-03-13 23:56:15 +0300

    renamed common frame's rank to normal

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,7 +1,7 @@
 class Frame < ApplicationRecord
   belongs_to :game
 
-  enum rank: %i[common strike spare]
+  enum rank: %i[normal strike spare]
 
   validates_numericality_of :score, less_than_or_equal_to: 30
 end
--- spec/models/frame_spec.rb
+++ spec/models/frame_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Frame, type: :model do
   it { should belong_to(:game) }
 
-  it { should define_enum_for(:rank).with_values(%i[common strike spare]) }
+  it { should define_enum_for(:rank).with_values(%i[normal strike spare]) }
   it { should validate_numericality_of(:score).is_less_than_or_equal_to(30) }
 end
